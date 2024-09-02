22007151267ffc7e63004b244ef3ba70952b667c
bennyhsieh <benny.hsieh@jyintgroup.com>
2024-09-02 18:48:01 +0800

    feat: update package

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { setCookie } from 'cookies-next';
+import Link from "next/link";
 import { useRouter } from 'next/navigation'
 
 import { Button } from "@/components/ui/button"
@@ -11,8 +12,11 @@ export default function Header() {
     setCookie('x-locale', lan)
     router.refresh()
   }
+
   return (
     <div>
+      <Button onClick={() => router.push('/about')}>About</Button>
+      <Button onClick={() => router.push('/')}>Home</Button>
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">Change Language</h2>
       <Button onClick={() => changeLanguage('en')}>en</Button>
       <Button onClick={() => changeLanguage('de')}>de</Button>
