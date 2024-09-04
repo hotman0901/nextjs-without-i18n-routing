2209d0c73e1b2fb27da6afde1c819a49b223f0e8
bennyhsieh <benny.hsieh@jyintgroup.com>
2024-09-04 16:24:05 +0800

    feat: update package

---
+++ src/app/slide/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+import '@egjs/react-flicking/dist/flicking.css';
+import "@egjs/react-flicking/dist/flicking-inline.css";
+
+import Flicking from '@egjs/react-flicking';
+import { useTranslations } from 'next-intl';
+
+// import PageLayout from '@/components/PageLayout';
+
+export default function App() {
+  const t = useTranslations('About');
+
+  return (
+    <Flicking
+      align="prev"
+      circular={true}
+      onMoveEnd={(e) => {
+        console.log(e);
+      }}
+    >
+      <div className="panel">1</div>
+      <div className="panel">2</div>
+      <div className="panel">3</div>
+    </Flicking>
+  );
+}
