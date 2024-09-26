4f0e6ba90f111446bf89ab244f2f09e88ca35f00
bennyhsieh <benny.hsieh@jyintgroup.com>
2024-09-26 16:38:59 +0800

    feat: update package

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,12 +6,12 @@ import {devices} from '@playwright/test';
 const PORT = process.env.CI ? 3001 : 3000;
 
 const config: PlaywrightTestConfig = {
-  retries: process.env.CI ? 1 : 0,
+  // retries: process.env.CI ? 1 : 0,
   testDir: './tests',
   projects: [
     {
-      name: 'chromium',
-      use: devices['Desktop Chrome']
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' }, // or 'chrome-beta'
     }
   ],
   fullyParallel: true,
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,9 +1,14 @@
-import {test as it} from '@playwright/test';
+// import {test as it} from '@playwright/test';
 
-it('uses the provided locale', async ({page}) => {
-  await page.goto('/');
-  page.getByRole('heading', {name: 'Home'});
+// it('uses the provided locale', async ({page}) => {
+//   await page.goto('http://localhost:3000/')
+// });
 
-  await page.goto('/');
-  page.getByRole('heading', {name: 'About'});
+import { expect,test } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
