7718b64c6736ab1bf8311237476913e7d7d3a9b0
bennyhsieh <benny.hsieh@jyintgroup.com>
2025-08-07 14:48:47 +0800

    feat: update

--- src/schemas/login.ts
+++ src/schemas/login.ts
@@ -1,15 +1,21 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const LoginFormSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required' }).superRefine((value, ctx) => {
-    if (value.length >= 7) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'The name is too long',
-      })
-    }
-  }),
+  name: z
+    .string()
+    .min(1, { message: 'Name is required' })
+    .superRefine((value, ctx) => {
+      if (value.length >= 7) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'The name is too long',
+        });
+      }
+    }),
   password: z
-    .string({ required_error: 'Password 為必填欄位' })
-    .min(6, { message: 'Message must be at least 6 characters.' })
-})
+    .string({
+      error: (issue) =>
+        issue.input === undefined ? 'This field is required' : 'Not a string',
+    })
+    .min(6, { message: 'Message must be at least 6 characters.' }),
+});
